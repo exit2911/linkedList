f240304fed40a4ce84e6a4cfba4b271e50f80520
Vy Ho <vyho0711@gmail.com>
2019-03-25 23:35:19 -0400

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,4 +1,7 @@
+"""
+print kth to last element in the linked list
 
+"""
 class Node:
     
     def __init__(self,data):
@@ -10,61 +13,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
-    def printList(self): #traverse a linked list
-        temp = self.head
-        while temp.next != None:
-            print(temp.data)
-            temp = temp.next
-    
-    def removeDuplicates(self):  # remove duplicate method for sorted list
-        temp = self.head 
-        if temp is None: 
-            return
-        while temp.next is not None: 
-            if temp.data == temp.next.data: 
-                temp.next = None
-                temp.next = temp.next.next 
-            else: 
-                temp = temp.next
-        
-     
-    
-    def removeDup(self):    # remove duplicate method for unsorted list
-        
-        elements = []
-        
-        temp = self.head #current node; starting at the head
-        
-        if temp.next is None:
-            return
-        while temp.next:
-            if temp.data in elements:
-               temp.next = None 
-               temp.next = temp.next.next
-            else:
-                
-                elements.append(temp.data)
-        
-                temp = temp.next
-
-        return self
-    
-    def remove_dup_followup(self):
-        
-        current = self.head #current node; starting at the head
-        
-        if current.next == None:
-            return
-        
-        while current.next:
-            if current.data == current.next.data:
-                
-                current.next = current.next.next
-                
-            else:
-                current = current.next
-                
-    
+   
       
     def displayK(self,k): #print kth to last element in the linked list
         
