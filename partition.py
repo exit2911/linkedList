567b18bce562b8976b37c4bd462107463ceddde3
Vy Ho <vyho0711@gmail.com>
2019-04-07 17:20:42 -0400

    Update partition.py

--- partition.py
+++ partition.py
@@ -13,4 +13,51 @@ tips:
 """
 
 
+class Node:
+    
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        
+    def printList(self): #traverse a linked list
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+    
+    def push(self, new_data): 
+        new_node = Node(new_data) #think of class as a bag. set new_node variable to a value
+        new_node.next = self.head #new node.next is now a copy of the current head
+        self.head = new_node  
+    
+    def assem(self):
+        the_list = self.head
+        llist = [1,2,3]
+        
+        for i in range(3):
+            
+            new_node = Node(llist[i])
+            
+            new_node.next = self.head
+            self.head = new_node
+            
+            
+        return the_list
+        
+        
+      
+        
+if __name__ == "__main__":
+    
+    the_list = LinkedList()
+    the_list.assem()
+    
+   
+    the_list.printList()
+    
 
