522598f1381524de058fe2671e4cb0eafa9bc94d
Vy Ho <vyho0711@gmail.com>
2019-04-07 21:08:47 -0400

    Update partition.py

--- partition.py
+++ partition.py
@@ -13,16 +13,6 @@ tips:
 """
 
 
-"""
-write code to partition a linked list around a value x such that all nodes 
-less than x come before x and all nodes greater than x are on the right
-if x is within the linked list, x can be anywhere in the right partition 
-tips:
-    
-    use 2 linked lists, one with values less than x and one with values that are equal to or greater than x
-    iterate through the entire linked list
-- randomly add one node in front of another. then assemble the list without initialization
-"""
 
 
 class Node:
