02c9a8edba721d8218f1c2c5627ada89a468135a
Vy Ho <vyho0711@gmail.com>
2019-04-10 23:32:38 -0400

    Create sum_lists.py

---
+++ sum_lists.py
@@ -0,0 +1,12 @@
+"""
+add sum list
+
+2 linked lists of 2 reversed numbers. each node has one digit. write an algorithm to add
+
+adding 2 numbers require starting at the last digits. hence, we don't have to reverse the 2 linked lists. we only need to reverse the result linked list
+
+modulo works for all nodes. no need to distinguish the location of the carrying digit
+
+"""
+
+
